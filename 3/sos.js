cea3e391efb3b4884ee7512b493778b35e223d25
Belli Charan <222931278+CharanBelli@users.noreply.github.com>
2025-08-17 16:44:19 +0530

    Update sos.js

--- 3/sos.js
+++ 3/sos.js
@@ -231,7 +231,7 @@ io.on('connection', (socket) => {
 
 // ---- Start server ----
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 
